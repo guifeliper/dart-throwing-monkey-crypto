05d3e00e7cb981f4d2b99f3295885833b32fcc6c
Guilherme Reis <guifeliper@gmail.com>
2023-05-02 21:16:29 +0200

    fix: update folder

---
+++ apps/monkey-crypto/utils/selectTokens.ts
@@ -0,0 +1,9 @@
+import  { MersenneTwister19937, sample } from "random-js";
+
+const selectTokens = (list: any[], quantity: number) => {
+  const engine = MersenneTwister19937.autoSeed();
+  
+  return sample(engine, list, quantity);
+};
+
+export default selectTokens;
