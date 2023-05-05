465134e514a316af9b3eb8112f6ff9148a2d37c9
Guilherme Reis <guifeliper@gmail.com>
2023-05-05 21:52:08 +0200

    feat: removing anything that is not the host
    fix: undefinied variables
    
    Fix: token list when it is empty
    
    chore: adding info in the answer
    
    fix: the https issue
    
    chore: display token list
    
    clean

--- apps/monkey-crypto/app/api/generateTokens/route.ts
+++ apps/monkey-crypto/app/api/generateTokens/route.ts
@@ -5,9 +5,11 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
-export const revalidate = 60;
+export const revalidate = 10;
 export async function GET(request: Request) {
-  const url = `${process.env.COINMARKETCAP_URL}/v1/cryptocurrency/listings/latest?limit=100` ?? "";
+  const url =
+    `${process.env.COINMARKETCAP_URL}/v1/cryptocurrency/listings/latest?limit=100` ??
+    "";
   var options = {
     headers: {
       "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API ?? "",
@@ -18,27 +20,34 @@ export async function GET(request: Request) {
   const res = await fetch(url, options);
   const data = await res.json();
 
-  const tokensList = data?.data.map((token: any) => {
-    return {
-      name: token.name,
-      symbol: token.symbol,
-      priceAtDrawn: token.quote["USD"].price,
-      priceAtContest: 0,
-      category: "100",
-      timeframe: getYearWeekString(),
-    };
-  });
+  const tokensList =
+    data?.data?.map((token: any) => {
+      return {
+        name: token.name,
+        symbol: token.symbol,
+        priceAtDrawn: token.quote["USD"].price,
+        priceAtContest: 0,
+        category: "100",
+        timeframe: getYearWeekString(),
+      };
+    }) ?? [];
 
+  if (tokensList?.length == 0) {
+    return NextResponse.json(
+      { message: "Error on token list" },
+      { status: 500 }
+    );
+  }
   const selectedTokens = selectTokens(tokensList, 10);
   return await addTokensDrawn(selectedTokens);
 }
 
 async function addTokensDrawn(data: any) {
   try {
-    const result = await prisma.tokenDrawn.createMany({ data });
-    return NextResponse.json({ fields: "Add Successfully" }, { status: 200 });
+    await prisma.tokenDrawn.createMany({ data });
+    return NextResponse.json({ message: "Add Successfully" }, { status: 200 });
   } catch (error) {
     console.error("Request error", error);
-    return NextResponse.json({ fields: "Error on add" }, { status: 500 });
+    return NextResponse.json({ message: "Error on add" }, { status: 500 });
   }
 }
---
+++ apps/monkey-crypto/middleware.ts
@@ -0,0 +1,21 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  matcher: "/api/:path*",
+};
+
+export function middleware(request: NextRequest, res: NextResponse) {
+  const referer = request.headers.get("referer");
+  const host = request.headers.get("host");
+  const frontendOrigin = `${host}`;
+
+  if (!referer || !referer?.includes(frontendOrigin)) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  return NextResponse.next();
+}
+
+export default middleware;
