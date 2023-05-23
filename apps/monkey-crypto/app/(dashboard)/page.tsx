8e84d8ef8a95f7774abbbe434d8e7594e926df94
Guilherme Reis <guifeliper@gmail.com>
2023-05-23 22:42:37 +0200

    feature: hard coded data

--- apps/monkey-crypto/app/(dashboard)/page.tsx
+++ apps/monkey-crypto/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { WeekTokens } from "@/components/weekTokens";
-import prisma from "@/lib/prisma";
+import prisma, { TokenDrawns } from "@/lib/prisma";
 import getYearWeekString from "@/utils/getYearWeekString";
 import { LineChart } from "@/components/line-chart";
 export const metadata = {
@@ -7,18 +7,21 @@ export const metadata = {
 };
 
 export default async function Home() {
+  const tokenDrawn = TokenDrawns(prisma.tokenDrawn)
   const getCurrentWeek = getYearWeekString();
-  const weekTokens = await prisma.tokenDrawn.findMany({
+  const weekTokens = await tokenDrawn.findMany({
     where: {
       category: "100",
       timeframe: getCurrentWeek,
     },
   });
+  
+  const calculatedInvesment = await tokenDrawn.calculateInvestment();
 
   return (
     <div className="grid grid-cols-3 gap-0">
       <div className="col-span-2 p-4 bg-gray-200 dark:bg-gray-800">
-        <LineChart />
+        <LineChart data={calculatedInvesment} />
       </div>
       <div className="p-4 bg-gray-300 dark:bg-gray-500">
         <WeekTokens data={weekTokens} />
