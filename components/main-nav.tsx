5d4c6f308d8e0a31d65f03dca6556fdf8e89abe2
Guilherme Reis <guifeliper@gmail.com>
2023-07-03 22:15:36 +0200

    fix: logo size

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -50,7 +50,7 @@ export function MainNav({ items, children }: MainNavProps) {
         className="flex items-center space-x-2 md:hidden"
         onClick={() => setShowMobileMenu(!showMobileMenu)}
       >
-        {showMobileMenu ? <Icons.close /> : <Icons.logo />}
+        {showMobileMenu ? <Icons.close /> : <Icons.logo className="h-6 w-6"/>}
         <span className="font-bold">Menu</span>
       </button>
       {showMobileMenu && items && (
