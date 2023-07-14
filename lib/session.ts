14f818dd0e656955f5f151c42a9cdadd6e74053a
Guilherme Felipe Reis <guifeliper@gmail.com>
2023-07-14 16:11:56 +0200

    fix: remove unused types (#17)

--- lib/session.ts
+++ lib/session.ts
@@ -1,13 +1,9 @@
 import { getServerSession } from "next-auth/next"
 
 import { authOptions } from "@/lib/auth"
-import { DefaultSession } from "next-auth"
-interface UserSession extends NonNullable<DefaultSession["user"]> {
-  id: string
-}
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions)
 
-  return session?.user as UserSession
+  return session?.user
 }
