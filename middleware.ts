ee7c1c158d0a605ef50ef4660c0c91db505dc5c4
Guilherme Reis <guifeliper@gmail.com>
2023-07-21 16:37:07 +0200

    feature: multiple middleware part 1

--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,19 @@
-import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
+import { withAuthPath } from "./middlewares/withAuthPath"
+import { withLogging } from "./middlewares/withLogging"
 
-export default withAuth(
-  async function middleware(req) {
-    const token = await getToken({ req })
-    const isAuth = !!token
-    const isAuthPage =
-      req.nextUrl.pathname.startsWith("/login") ||
-      req.nextUrl.pathname.startsWith("/register")
+const middlewares = withLogging(withAuthPath)
 
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
-      }
-
-      return null
-    }
-
-    if (!isAuth) {
-      let from = req.nextUrl.pathname
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search
-      }
-
-      return NextResponse.redirect(
-        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-      )
-    }
-  },
-  {
-    callbacks: {
-      async authorized() {
-        // This is a work-around for handling redirect on auth pages.
-        // We return true here so that the middleware function above
-        // is always called.
-        return true
-      },
+export default withAuth(middlewares, {
+  callbacks: {
+    async authorized() {
+      // This is a work-around for handling redirect on auth pages.
+      // We return true here so that the middleware function above
+      // is always called.
+      return true
     },
-  }
-)
+  },
+})
 
 export const config = {
   matcher: ["/dashboard/billing", "/login", "/register"],
