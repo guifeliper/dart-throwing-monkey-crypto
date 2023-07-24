192471d26c917576cad8639aae0aaea1dc3339c4
Guilherme Reis <guifeliper@gmail.com>
2023-07-24 16:55:39 +0200

    feature: add the redirection link after success login

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
-import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
 import createIntlMiddleware from "next-intl/middleware"
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
 const locales = ["en", "pt"]
 const publicPages = ["/", "/login", "/register", "/dashboard", "/pricing"]
@@ -12,38 +11,12 @@ const intlMiddleware = createIntlMiddleware({
 })
 
 const authMiddleware = withAuth(
-  // Note that this callback is only invoked if
-  // the `authorized` callback has returned `true`
-  // and not for pages listed in `pages`.
   async function onSuccess(req) {
-    const token = await getToken({ req })
-    const isAuth = !!token
-    const isAuthPage =
-      req.nextUrl.pathname.startsWith("/login") ||
-      req.nextUrl.pathname.startsWith("/register")
-
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
-      }
-
-      return intlMiddleware(req)
-    }
-
-    if (!isAuth) {
-      let from = req.nextUrl.pathname
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search
-      }
-
-      return NextResponse.redirect(
-        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-      )
-    }
+    return intlMiddleware(req)
   },
   {
     callbacks: {
-      authorized: () => true,
+      authorized: ({ token }) => token != null,
     },
     pages: {
       signIn: "/login",
