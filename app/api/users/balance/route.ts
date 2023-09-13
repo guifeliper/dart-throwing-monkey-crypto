d14df5be0cb91793c69fd2a97ccd338118556fa8
Guilherme Reis <guifeliper@gmail.com>
2023-09-13 23:00:23 +0200

    fix: correct balance

--- app/api/users/balance/route.ts
+++ app/api/users/balance/route.ts
@@ -1,19 +1,20 @@
 import { env } from "@/env.mjs"
-import { authOptions } from "@/lib/auth"
+import { calculateAssetWeights } from "@/lib/calculateAssetWeights"
 import { mergeBalanceObjects } from "@/lib/utils"
 import { Kraken as PrivateKraken } from "@/services/kraken"
-import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 import { Kraken } from "node-kraken-api"
 import { z } from "zod"
 
 export async function GET() {
   try {
-    const session = await getServerSession(authOptions)
+    // const session = await getServerSession(authOptions)
 
-    if (!session?.user || !session?.user.email) {
-      return new Response(null, { status: 403 })
-    }
+    // if (!session?.user || !session?.user.email) {
+    //   return new Response(JSON.stringify({ totalBalanceFIAT: 0, data: [] }), {
+    //     status: 403,
+    //   })
+    // }
     const quote = "ZUSD"
     const privateKraken = new PrivateKraken()
     const kraken = new Kraken({
@@ -26,42 +27,41 @@ export async function GET() {
     let totalBalanceUsd = 0
 
     const data = await Promise.all(
-      Object.entries(balance).map(async ([currency, value]) => {
-        const currencyValue = currency.split(".")[0]
-        if (currencyValue === "ZUSD") {
-          return {
-            asset: currencyValue,
-            quantity: value,
-            pair: null,
-            totalFIAT: 1,
-          }
-        } else {
+      Object.entries(balance)
+        .filter(([currency, value]) => Number(value) !== 0)
+        .map(async ([currency, value]) => {
+          const currencyValue = currency.split(".")[0]
           const assetPairs = Object.values(assets).filter(
             (asset) =>
               (asset as any).base === currencyValue &&
               (asset as any).quote === quote
           )[0]?.["altname"]
+          console.log("assetPairs", assetPairs, currencyValue, quote)
 
-          const ticker = await privateKraken.ticker(assetPairs)
-          const lastPrice = (
-            Object.entries((ticker as any).result) as any
-          )[0][1].c[0]
-          console.log(currency, value, assetPairs, lastPrice)
+          let lastPrice = 0
+          if (!assetPairs) {
+            lastPrice = 1
+          } else {
+            const ticker = await privateKraken.ticker(assetPairs)
+            lastPrice = (Object.entries((ticker as any).result) as any)[0][1]
+              .c[0]
+          }
+
           totalBalanceUsd += parseFloat(value) * lastPrice
-
           return {
             asset: currencyValue,
-            quantity: value,
+            quantity: parseFloat(value),
             pair: assetPairs,
             value: lastPrice,
             totalFIAT: parseFloat(value) * lastPrice,
           }
-        }
-      })
+        })
     )
 
+    const noDuplicates = mergeBalanceObjects(data)
+    const weighted = calculateAssetWeights(noDuplicates, totalBalanceUsd)
     return NextResponse.json(
-      { totalBalanceFIAT: totalBalanceUsd, data: mergeBalanceObjects(data) },
+      { totalBalanceFIAT: totalBalanceUsd, data: weighted },
       { status: 200 }
     )
   } catch (error) {
